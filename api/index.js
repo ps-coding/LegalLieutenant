ca1ad5ac4bcced36a77eb8a4cf6f1b63b7cd6762
ps-coding <95548088+ps-coding@users.noreply.github.com>
2024-08-07 08:45:08 -0400

    Improve advanced regex: require a dash or colon for the 35 character allowance to be triggered

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ const client = new openai.OpenAI({
 });
 
 const groupSections = (documentContent) => {
-  let splitSections = documentContent.replaceAll(/^((\s*((#+.*)|(\*+.*))(\r?\n)+)+)/gm, "[;break;]$1").replaceAll(/^((\s*\**\s*(((Part)|(Article)|(Section)|(Preamble)|(Clauses)|(Definitions)|(Clause)|(Definition))\.?\s*((([0-9]+)|([IVXLCDM]+)|([A-Z]+))\.?)?\s*.{0,35})\**\s*(\r?\n)+)+)/gmi, "[;break;]$1").replaceAll("\n", " ").split("[;break;]");
+  let splitSections = documentContent.replaceAll(/^((\s*((#+.*)|(\*+.*))(\r?\n)+)+)/gm, "[;break;]$1").replaceAll(/^((\s*\**\s*(((Part)|(Article)|(Section)|(Preamble)|(Clauses)|(Definitions)|(Clause)|(Definition))\.?\s*((([0-9]+)|([IVXLCDM]+)|([A-Z]+))\.?)?\s*(((:)|(-)|(–)|(—))+\s*.{0,35})?)\**\s*(\r?\n)+)+)/gmi, "[;break;]$1").replaceAll("\n", " ").split("[;break;]");
 
   if (splitSections.length == 1) {
     splitSections = splitSections[0].replaceAll(/([\.\?!])/g, "$1\n").split("\n");
